28f0e40d66ebb1b953be151d9a81cf6de8973dd5
Sean Schelvis <sean.schelvis@hotmail.com>
2023-01-10 09:10:59 +0100

    added backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import chats from "./data/data.js";
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+})
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Listening on PORT ${PORT}`));
+

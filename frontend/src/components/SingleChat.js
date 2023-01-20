670be720121fc38664718bb1964df2575eae7f14
Sean Schelvis <sean.schelvis@hotmail.com>
2023-01-20 09:30:43 +0100

    Added box where messages will be dislayed

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -32,10 +32,6 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
                 {/* <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} /> */}
               </div>
             )}
-
-            <div>
-              {/* Messages here */}
-            </div>
           </div>
 
         ) : (
@@ -48,6 +44,22 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
           </Container>
         )
       }
+
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        padding: "3%",
+        background: "#E8E8E8",
+        width: "100%",
+        height: "100%",
+        minHeight:"70vh",
+        borderRadius: "5px",
+        overflow: "hidden",
+        marginTop:"2%"
+      }}>
+        {/* Messages here */}
+      </div>
     </>
   )
 }

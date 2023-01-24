b77a996d4a21bef47c02c14d63b8f17d96df2542
Sean Schelvis <sean.schelvis@hotmail.com>
2023-01-24 14:08:20 +0100

    Finally fixed messaging bug (i hope)

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ import chatRoutes from "./routes/chatRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import { Server } from "socket.io";
 
-const app = express();
 dotenv.config();
 connectDB();
+const app = express();
 
 app.use(express.json());
 
@@ -23,45 +23,48 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Listening on PORT ${PORT}`.yellow.bold));
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000"
-  }
+    origin: "http://localhost:3000",
+  },
 });
 
 io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
   socket.on("setup", (userData) => {
     socket.join(userData._id);
-    socket.emit("connected")
-  })
+    socket.emit("connected");
+  });
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    console.log("User Joined Room: " + room);
+    console.log("User joined Room: " + room);
   });
 
-  socket.on("typing", (room) => socket.in(room).emit("typing"))
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"))
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
   socket.on("new message", (newMessageReceived) => {
-    let chat = newMessageReceived.chat;
+    var chat = newMessageReceived.chat;
 
-    if (!chat.users) {
-      return console.log("chat.users not defined")
-    }
+    if (!chat.users) return console.log("chat.users not defined");
 
     chat.users.forEach((user) => {
       if (user._id == newMessageReceived.sender._id) return;
 
-      socket.in(user._id).emit("message received", newMessageReceived)
-    })
-  })
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
 
   socket.off("setup", () => {
     console.log("USER DISCONNECTED");
-    socket.leave(userData._id)
-  })
-})
+    socket.leave(userData._id);
+  });
+});
